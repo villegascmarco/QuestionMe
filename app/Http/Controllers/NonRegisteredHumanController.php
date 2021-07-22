5af9a4e7bc1c6e10afce33993d1a52791419dbef
Villegas <villegascmarco@gmail.com>
2021-07-22 09:37:43 -0500

    Creación de modelo y controlador

---
+++ app/Http/Controllers/NonRegisteredHumanController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\non_registered_human;
+use Illuminate\Http\Request;
+
+class NonRegisteredHumanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\non_registered_human  $non_registered_human
+     * @return \Illuminate\Http\Response
+     */
+    public function show(non_registered_human $non_registered_human)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\non_registered_human  $non_registered_human
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, non_registered_human $non_registered_human)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\non_registered_human  $non_registered_human
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(non_registered_human $non_registered_human)
+    {
+        //
+    }
+}
---
+++ app/Models/non_registered_human.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class non_registered_human extends Model
+{
+    use HasFactory;
+}
