d30556b7451fbd16b5726399af7edb106ea87897
MoiMorua <57070474+MoiMorua@users.noreply.github.com>
2021-08-02 17:34:59 -0500

    Se corrigen las rutas

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,10 @@ use App\Http\Middleware\Role;
 |
 */
 
-
+//MainPage
 Route::get('/', 'SiteController@frontpage');
-
 //AUTH
 Route::get('/login', 'SiteController@login')->name('login');
-
 Route::get('/signup', 'SiteController@signup')->name('signup');
 Route::post('/auth', [LoginController::class, 'authenticate']);
 Route::post('/registration', [LoginController::class, 'signUp'])->name('registration');
@@ -32,7 +30,7 @@ Route::post('/authenticateWithSocialMedia', [LoginController::class, 'authentica
 Route::get('/logout', [LoginController::class, 'logout']);
 //Main page
 Route::get('/','SiteController@frontpage');
-
+//Users
 Route::get('/users/userNameTaken/{name}',  [userController::class, 'userNameTaken']);
 Route::get('/users/emailUsed/{name}',  [userController::class, 'emailUsed']);
 
@@ -44,38 +42,17 @@ Route::group(['middleware'=>['auth']], function(){
         Route::get('/users/roleFind/{role}',  [userController::class, 'roleFind']);
         Route::resource('users','userController',["except" => ['destroy']]);
         Route::get('/user','SiteController@user');
+        //user-role
+        Route::post("/user_role/desactivate/{id}", [user_roleController::class, 'desactivate']);
+        Route::resource('user_role','user_roleController', ["except" => ['destroy']]);
     });
-
-    Route::resource('questions', 'questionController');
-    //users
-    Route::post('/users/desactivate/{id}', [userController::class, 'desactivate']);
-    Route::post("/users/activate/{id}", [userController::class, 'activate']);
-    Route::get('/users/roleFind/{role}',  [userController::class, 'roleFind']);
-    Route::resource('users','userController',["except" => ['destroy']]);
-    //human
-    Route::post("/human/desactivate/{id}", [humanController::class, 'desactivate']);
-    Route::resource('human','humanController', ["except" => ['destroy']]);
-    //user-role
-    Route::post("/user_role/desactivate/{id}", [user_roleController::class, 'desactivate']);
-    Route::resource('user_role','user_roleController', ["except" => ['destroy']]);
+    Route::resource('questions', 'questionController');        
     //Dashboard
     Route::get("/dashboard", 'SiteController@dashboard');
     // Resource::
     Route::resource('questions', 'questionController');
+    //Quiz
+    Route::get('/quiz', 'SiteController@quiz');
+    Route::get('/new-quiz', 'SiteController@quizCreation');
 });
-
-Route::get('/user', 'SiteController@user');
-
-Route::get('/quiz', 'SiteController@quiz');
-
-Route::get('/new-quiz', 'SiteController@quizCreation');
-
-// Route::group(['middleware'=>['auth']], function(){
-
-    // });
-
-
-    //Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
-
-//})->name('home');
 
