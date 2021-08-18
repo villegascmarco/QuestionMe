73c49e0c158fa95f1dbc84bb861aa9263d4dc0ca
MoiMorua <57070474+MoiMorua@users.noreply.github.com>
2021-08-18 18:54:28 -0500

    Se agregan los basicos para el funcionamiento del dashboard

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -81,8 +81,13 @@ class SiteController extends Controller
 
     public function dashboard(){
         $title = "Dashboard | QuestionMe!";
-        $styleSheets = [];
-        $jsDocs = [];
+        $styleSheets = [
+            array('local'=>true,'route'=>'css/dashboard.css'),
+            array('local'=>true,'route'=>'css/module.css')
+        ];
+        $jsDocs = [
+            array('local'=>true,'route'=>'js/modules/dashboard/dashboard.js')
+        ];
         return view('dashboard',compact("title","styleSheets","jsDocs"));
 
     }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,13 @@
 @extends('main')
 @section('content')
-Dashboard
+
+<header class="module-header">
+    <h1 class="module-title">Bienvenido</h1>
+</header>
+
+<main class="main-container">
+    <header></header>
+</main>
+
+
 @endsection
